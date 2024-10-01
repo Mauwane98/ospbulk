e536e5fce8cf4e1c46dee6d8eabeea4b1b6264e2
Olerato Mauwane <oleratodichaba44@gmail.com>
2024-10-01 20:59:48 +0200

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Smooth scroll for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// JavaScript for toggling the menu
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
